644eeec4e41816ab40edddc0ea9411fe32cbaec4
Pranay Rane <pranayrane029@gmail.com>
2025-01-11 18:29:51 +0530

    Day-3: User controller and routes

--- Day-3/src/controller/user.controller.js
+++ Day-3/src/controller/user.controller.js
@@ -1,6 +1,15 @@
 const userModel = require('../models/user.model')
 
+const {validationResult} = require('express-validator')
+
 module.exports.registerUserController = async(req,res)=>{
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({error:errors.array()})
+    }
+
     try {
         const {username,password,email} = req.body;
 
--- Day-3/src/routes/user.routes.js
+++ Day-3/src/routes/user.routes.js
@@ -2,7 +2,14 @@ const {Router} = require('express');
 const router = Router();
 const userController  = require('../controller/user.controller');
 
+const {body } = require('express-validator')
 
-router.post('/register', userController.registerUserController);
+
+
+
+router.post('/register',
+    body("username").isString().withMessage("username must be a string")
+    .isLength({min:3}).withMessage('username must be greater than 3 char'),
+    userController.registerUserController);
 
 module.exports = router;
